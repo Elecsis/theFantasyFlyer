5528fdce0cc2ea124bac7de442f0c0f8dbbb0d58
Lex Pomales <lex@Lexs-Mac-mini.attlocal.net>
2024-07-04 21:58:04 -0400

    Signout issue bug fix#3

--- src/app/componets/LogInOutButton.tsx
+++ src/app/componets/LogInOutButton.tsx
@@ -17,6 +17,3 @@ export default  function LogInOutButton () {
     </>
     
 }
-
-
-//<div onClick={() => signOut({callbackUrl: `${window.location.origin}/login`}) } className="w-20 text-xs bg-lime-500 hover:bg-lime-500 p-2 text-center rounded text-white lg:text-lg">Log Out</div>
--- src/app/myAccount/page.tsx
+++ src/app/myAccount/page.tsx
@@ -1,7 +1,7 @@
 import { authOptions } from "@/utils/auth";
 import { getServerSession } from "next-auth";
-import { signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { SignOut } from "../componets/signoutButton";
 
 
 export default async function MyAccount() {
@@ -17,7 +17,7 @@ export default async function MyAccount() {
        
             <div className="flex h-52 w-72 rounded-lg bg-white text-center  items-center flex-col justify-evenly ">
                 <h1 className="pb-[8%] text-2xl">My Account</h1>
-                <div onClick={() => signOut() } className="w-20 text-xs bg-lime-500 hover:bg-lime-500 p-2 text-center rounded text-white lg:text-lg">Log Out</div>
+                <SignOut/>
             </div> 
         </main>
     )
